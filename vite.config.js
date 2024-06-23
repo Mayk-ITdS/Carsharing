3192eb6fd47f17a1f3ec1b894151b4c259caea54
Majk_Z-IT <156111094+Mayk-ITdS@users.noreply.github.com>
2024-06-23 09:39:07 +0200

    Starting new project

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+
+/**
+ * Path to exercise folder
+ */ 0;
+const exercisePath =
+  "07_Dzien_4/01_Tworzenie_ukladu_strony_-_ciag_dalszy/01_Car_Sharing";
+
+/**
+ * Don't change those lines below
+ */
+export default defineConfig({
+  root: exercisePath,
+  server: {
+    port: 3000,
+  },
+});
