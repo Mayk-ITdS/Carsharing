9d3e666e57467fdc90494e88fc1b518c24f603e5
Majk_Z-IT <156111094+Mayk-ITdS@users.noreply.github.com>
2024-06-24 22:48:03 +0200

    Last changes

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import { defineConfig } from "vite";
 /**
  * Path to exercise folder
  */ 0;
-const exercisePath =
-  "07_Dzien_4/01_Tworzenie_ukladu_strony_-_ciag_dalszy/01_Car_Sharing";
+const exercisePath = "01_Car_Sharing";
 
 /**
  * Don't change those lines below
